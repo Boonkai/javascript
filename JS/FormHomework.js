62950e91fac0e2250ad764fed3c7abeb8cbbaeb5
Boon Kai <boonkai.yeam@gmail.com>
2021-03-16 00:33:29 +0800

    first commit

---
+++ JS/FormHomework.js
@@ -0,0 +1,15 @@
+/*Add the JavaScript here for the function billingFunction(). 
+ It is responsible for setting and clearing the fields in Billing Information */
+
+ function billingFunction(){
+     if(document.getElementById('same').checked){
+         var shipName = document.getElementById('shippingName').value;
+         var shipZip = document.getElementById('shippingZip').value
+         document.getElementById('billingName').value=shipName;
+         document.getElementById('billingZip').value=shipZip;
+     }
+     else{
+        document.getElementById("billingName").value="";
+        document.getElementById('billingZip').value="";
+     }
+ }
